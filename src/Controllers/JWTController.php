dd81f685701312e5bb52d1ea2918d9f6924fe7fa
MrFrost-Nv27 <nazruelyourgoodfriend@gmail.com>
2023-08-17 22:07:05 +0700

    register

--- src/Controllers/JWTController.php
+++ src/Controllers/JWTController.php
@@ -5,6 +5,9 @@ namespace Mrfrost\JWT\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\HTTP\Response;
+use CodeIgniter\Shield\Authentication\Passwords;
+use CodeIgniter\Shield\Entities\User;
+use CodeIgniter\Shield\Exceptions\ValidationException;
 use Mrfrost\JWT\Authentication\JWTAuthenticator;
 use Mrfrost\JWT\Filters\JWTFilter;
 
@@ -27,8 +30,8 @@ class JWTController extends BaseController
             return $this->failValidationErrors($this->validator->getErrors());
         }
 
-        $credentials             = $this->request->getPost(setting('Auth.validFields'));
-        $credentials             = array_filter($credentials);
+        $credentials = $this->request->getPost(setting('Auth.validFields'));
+        $credentials = array_filter($credentials);
         $credentials['password'] = $this->request->getPost('password');
 
         /** @var JWTAuthenticator $authenticator */
@@ -43,8 +46,60 @@ class JWTController extends BaseController
         $jwe = $result->extraInfo();
 
         return $this->respond([
-            'identity'  => auth()->user()->username,
-            'token'     => $jwe,
+            'identity' => auth()->user()->username,
+            'token' => $jwe,
+        ]);
+    }
+
+    /**
+     * Attempts to log the user in.
+     */
+    public function registerAction(): Response
+    {
+        // Validate here first, since some things,
+        // like the password, can only be validated properly here.
+        $users = model(setting('Auth.userProvider'));
+        $rules = $this->getValidationRules();
+
+        if (!$this->validate($rules)) {
+            return $this->failValidationErrors($this->validator->getErrors());
+        }
+
+        $allowedPostFields = array_keys($rules);
+        $user = new User();
+        $user->fill($this->request->getPost($allowedPostFields));
+
+        // Workaround for email only registration/login
+        if ($user->username === null) {
+            $user->username = null;
+        }
+
+        try {
+            $users->save($user);
+        } catch (ValidationException $e) {
+            return $this->failValidationErrors($users->errors());
+        }
+
+        // To get the complete user object with ID, we need to get from the database
+        $user = $users->findById($users->getInsertID());
+
+        try {
+            $token = jwt()->getJWTService()
+                ->create(
+                    json_encode(
+                        payload($user)
+                    )
+                );
+        } catch (\Throwable $th) {
+            return new Result([
+                'success' => false,
+                'reason' => $th->getMessage(),
+            ]);
+        }
+
+        return $this->respond([
+            'identity' => auth()->user()->fullname,
+            'token' => $token,
         ]);
     }
 
@@ -56,7 +111,7 @@ class JWTController extends BaseController
     public function testAttempt()
     {
         $permission = [
-            'admin.access'
+            'admin.access',
         ];
         $filter = new JWTFilter();
         $this->request->setHeader(setting('Auth.authenticatorHeader')['tokens'] ?? 'Authorization', $this->request->getPost('token'));
@@ -70,11 +125,11 @@ class JWTController extends BaseController
 
         // jwt profile
         return $this->respond([
-            'success'  => true,
+            'success' => true,
             'JWT Type' => jwt()->getJWTService()->jwtType,
-            'token'    => jwt()->getJWTService()->serialize(),
-            'user'     => auth()->getUser(),
-            'group'    => auth()->getUser()->getGroups(),
+            'token' => jwt()->getJWTService()->serialize(),
+            'user' => auth()->getUser(),
+            'group' => auth()->getUser()->getGroups(),
             'permisson' => auth()->getUser()->getPermissions(),
         ]);
     }
@@ -87,18 +142,26 @@ class JWTController extends BaseController
      */
     protected function getValidationRules(): array
     {
-        return setting('Validation.login') ?? [
-            // 'username' => [
-            //     'label' => 'Auth.username',
-            //     'rules' => config('AuthSession')->usernameValidationRules,
-            // ],
+        $registrationEmailRules = array_merge(
+            config('AuthSession')->emailValidationRules,
+            [sprintf('is_unique[%s.secret]', $this->tables['identities'])]
+        );
+
+        return setting('Validation.registration') ?? [
+            'fullname' => [
+                'label' => 'Auth.username',
+                'rules' => $registrationUsernameRules,
+            ],
             'email' => [
                 'label' => 'Auth.email',
-                'rules' => config('AuthSession')->emailValidationRules,
+                'rules' => $registrationEmailRules,
             ],
             'password' => [
                 'label' => 'Auth.password',
-                'rules' => 'required',
+                'rules' => 'required|' . Passwords::getMaxLengthRule() . '|strong_password[]',
+                'errors' => [
+                    'max_byte' => 'Auth.errorPasswordTooLongBytes',
+                ],
             ],
         ];
     }
